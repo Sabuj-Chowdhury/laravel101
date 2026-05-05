51f8ab7bc3b65ab8920cb0cd2bb865592d52cdc2
Sabuj-Chowdhury <csabuj73@gmail.com>
2026-05-05 10:29:14 +0600

    make controller and function and use it in the route

--- api001/routes/api.php
+++ api001/routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/hello', function () {
+    return ["message" => "hello laravel!"];
+});
+
+Route::get('/posts', [PostController::class, 'index'])->name('post.index');
+
+Route::post('/posts', [PostController::class, 'store'])->name('post.store');
+
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('post.show');
